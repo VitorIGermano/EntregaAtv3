c722ce474fc86dcba35775164b493f48034ca917
VitorIGermano <61995326+VitorIGermano@users.noreply.github.com>
2020-06-07 16:38:05 -0300

    Add files via upload

--- modelo/Contato.js
+++ modelo/Contato.js
@@ -1,9 +1,16 @@
+
 class Contato {
-    constructor(key, nome, celular, foto) {
-        this.key = key;
+
+    constructor(id, nome, telefone, imagemURI, latitude, longitude, data){
+
+        this.id = id;
         this.nome = nome;
-        this.celular = celular;
-        this.foto = foto;
+        this.telefone = telefone;
+        this.imagemURI = imagemURI;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.data = data;
+
     }
 }
 
